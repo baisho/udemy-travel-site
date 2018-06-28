e6cecbf638f2eb10fb43e73816e01dd6044b3c2c
Bánk <mienkapalya7@gmail.com>
2018-06-28 14:28:32 +0200

    Tools installed. Starting CSS. Section 5, Lecture 13.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulpVar = require("gulp"),
+watch = require("gulp-watch"),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+cssVars = require("postcss-simple-vars"),
+nested = require("postcss-nested");
+
+
+gulpVar.task("default", function() {
+    console.log("Hooray - you created a Gulp task!");
+});
+
+gulpVar.task("html", function() {
+    console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulpVar.task("styles", function() {
+    return gulpVar.src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssVars, nested, autoprefixer]))
+    .pipe(gulpVar.dest("./app/temp/styles"));
+});
+
+gulpVar.task("watch", function() {
+
+    watch("./app/index.html", function() {
+        gulpVar.start("html");
+    });
+
+    watch("./app/assets/styles/**/*.css", function() {
+        gulpVar.start("styles");
+    });
+
+});
