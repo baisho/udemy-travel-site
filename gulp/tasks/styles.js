d47582116c719927db2ca2116969460775ef6899
Bánk <mienkapalya7@gmail.com>
2018-07-02 17:38:56 +0200

    Creating reusable blocks. S8L23.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,14 +3,15 @@ postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssVars = require("postcss-simple-vars"),
 nested = require("postcss-nested"),
-cssImport = require("postcss-import");
+cssImport = require("postcss-import"),
+mixins = require("postcss-mixins");
 
 gulpVar.task("styles", function() {
     return gulpVar.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
     .on("error", function(errorInfo) {
         console.log(errorInfo.toString());
-        this.emit("end");
+        this.emit("end"); /*we need this to tell watch that the task "styles" has been implemented successfully, no error occurred, and watch task can continue its functioning. Otherwise if watch task receives an error message from gulp it starts to panic and stops its working.*/
     })
     .pipe(gulpVar.dest("./app/temp/styles"));
 });
