b44985ecffc86cda6563c620e549ad1b214352a9
Bánk <mienkapalya7@gmail.com>
2018-07-05 19:22:03 +0200

    Header done for large screens. S13L38

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ autoprefixer = require("autoprefixer"),
 cssVars = require("postcss-simple-vars"),
 nested = require("postcss-nested"),
 cssImport = require("postcss-import"),
-mixins = require("postcss-mixins");
+mixins = require("postcss-mixins"),
+hexrgba = require("postcss-hexrgba");
 
 gulpVar.task("styles", function() {
     return gulpVar.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, hexrgba, autoprefixer]))
     .on("error", function(errorInfo) {
         console.log(errorInfo.toString());
         this.emit("end"); /*we need this to tell watch that the task "styles" has been implemented successfully, no error occurred, and watch task can continue its functioning. Otherwise if watch task receives an error message from gulp it starts to panic and stops its working.*/
