2ede968263aa0e4229adc161ac1c06e6a44e4e5e
Bánk <mienkapalya7@gmail.com>
2018-07-09 17:52:24 +0200

    Menu icon animation done.

--- app/assets/scripts/modules/Person.js
+++
@@ -1,9 +0,0 @@
-function Person(fullName, favColour) {
-    this.name = fullName;
-    this.favouriteColour = favColour;
-    this.greet = function() {
-        console.log("Hello, my name is " + this.name + " and my favourite colour is " + this.favouriteColour + ".");
-    }
-}
-
-module.exports = Person;
